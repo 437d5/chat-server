79c9bd31f3127227e3368ece5e9659682ee3a17e
437d5 <uladzislaukaplan@gmail.com>
2025-01-12 00:22:07 +0300

    db fix

--- auth_service_db/auth.sql
+++ auth_service_db/auth.sql
@@ -1,3 +1,7 @@
+CREATE DATABASE auth_db;
+
+\c auth_db;
+
 CREATE TABLE IF NOT EXISTS "users" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
@@ -6,3 +10,9 @@ CREATE TABLE IF NOT EXISTS "users" (
 
 INSERT INTO users (name, password) VALUES ('hello', '5d41402abc4b2a76b9719d911017c592');
 INSERT INTO users (name, password) VALUES ('bye', 'bfa99df33b137bc8fb5f5407d7e58da8');
+
+CREATE USER auth_service WITH PASSWORD 'auth_service_pass';
+GRANT CONNECT ON DATABASE auth_db TO auth_service;
+GRANT USAGE ON SCHEMA public TO auth_service;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO auth_service;
+GRANT INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO auth_service;
